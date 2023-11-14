cde3cce4a45b8c34185a3f79e92567c9fc085ac6
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-11-14 08:59:14 +0100

    HW 4 done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,65 @@
+class Key {
+  protected signature: number = Math.random();
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean = false;
+  public key: Key;
+  private tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      if (this.key.getSignature() === person.getKey().getSignature()) {
+        this.tenants.push(person);
+        console.log("Welcome!");
+      } else {
+        console.log("Invalid key!");
+      }
+    } else {
+      console.log("The door is closed!");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+      console.log("The door is open");
+    } else {
+      console.log("Invalid key! The door remains closed");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
